2091bfaa87b313dbd51162310cd7b70728544c46
ubbaayyyyy <fabianazhar726@gmail.com>
2023-10-31 21:12:16 +0700

    update files

--- src/components/Content4.jsx
+++ src/components/Content4.jsx
@@ -16,7 +16,7 @@ export const Content4 = () => {
                         <span className='block mt-6 mb-4 xl:mb-7 xl:mt-0 text-3xl xl:text-5xl xl:tracking-wider Font1'>
                             Nitro all fall long
                         </span>
-                        <span className='font-normal tracking-normal text-lg mb-6 xl:mb-7 xl:text-xl xl:tracking-wider xl:font-bold xl:leading-[3rem]'>
+                        <span className='tracking-normal font-normal xl:font-bold text-lg xl:text-2xl xl:leading-10 mb-6 xl:mb-9'>
                             Ready on tap, super-smooth Nitro Cold <br /> Brew.
                             For a silky twist, try Vanilla Sweet <br /> Cream
                             Nitro Cold Brew.
